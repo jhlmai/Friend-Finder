14795b9077e41d02aaf0cfdb6229bc99a12c9c39
Kathy Mai <kathymai@Kathys-MacBook-Air.local>
2018-01-12 00:07:44 -0800

    linked up files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,24 @@
-[
 {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
+    "scoreboard": [
+      {
+        "name": "harry potter",
+        "photo": "https://roadtovrlive-5ea0.kxcdn.com/wp-content/uploads/2017/11/harrypotter.jpg",
+        "scores": [1, 2, 4, 5, 3, 4, 5, 1, 2, 4]
+      },
+      {
+        "name": "luke skywalker",
+        "photo": "https://i.redd.it/2qmnb44sbt7z.jpg",
+        "scores": [3, 4, 5, 1, 1, 3, 5, 3, 5, 5]
+      },
+      {
+        "name": "frodo baggins",
+        "photo": "https://www.universeguide.com/pictures/frodobaggins.jpg",
+        "scores": [3, 3, 2, 1, 3, 3, 5, 3, 3, 1]
+      },
+      {
+        "name": "james kirk",
+        "photo": "http://4.bp.blogspot.com/-vc__p_18QqA/UpPVBZd86HI/AAAAAAAAZDI/gsisPXoU0zY/s1600/72280_f3.jpg",
+        "scores": [1, 3, 2, 1, 1, 5, 5, 5, 5, 5]
+      }
+    ]
   }
-]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,22 +1,56 @@
-
 module.exports = function(app) {
 	var fs = require('fs');
-	var ogData = {};
 
-// A GET route with the url /api/friends. 
+	var friends = require('./app/routing/htmlRoutes.js');
+	var apiRoutes = require('./app/routing/apiRoutes.js');
+
+
+
+	var surveyData = {};
+
+// GET route with the url `/api/friends`. 
 // This will be used to display a JSON of all possible friends.
 	app.get("/api/friends", function(req, res) {
 
-		fs.readFile('app/data/friends.json', function(err,data){
+		fs.readFile('app/data/friends.js', , function(err,data){
 			// console.log(typeof data, data);
 			var parsed = JSON.parse(data);
 			var arr = parsed.scoreboard;
 			console.log('arr',arr);
 			res.json(arr);
 		})
-    });
-    
+	});
 
-    // A POST routes /api/friends. This will be used to handle incoming survey results. 
-    // This route will also be used to handle the compatibility logic.
-    
+// POST routes `/api/friends`. 
+// This will be used to handle incoming survey results. 
+// This route will also be used to handle the compatibility logic. 
+	app.post('/api/friends', function(req,res){
+		console.log('post /api/friends ---------------------------');
+		var newData = req.body;
+		console.log('newData',newData);
+
+		fs.readFile('app/data/friends.json', function(err,data){
+			var parsed = JSON.parse(data);
+			surveyData = parsed;
+			var arr = parsed.scoreboard;
+
+			// Call function that will return most compatible.
+			var winners = findMatch(newData.scores,arr)
+
+			// Add the new data to the rest since the comparison is finished.
+			arr.push(newData);
+			var dataExport = JSON.stringify({scoreboard:arr});
+			fs.writeFile('app/data/friends.json',dataExport,'utf8',function(err,data){
+				if(err) throw err; 
+			})
+		}
+
+			// Convert each user's results into a simple array of numbers
+
+			// compare the difference between current user's scores 
+			// against those from other users, question by question. 
+			// Add up the differences to calculate the totalDifference.
+			// The closest match will be the user with the least amount of difference.
+
+			// Once you've found the current user's most compatible friend
+			// display the result as a modal pop-up.
